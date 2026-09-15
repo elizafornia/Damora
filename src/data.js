900c625582a1c3be0ece8417c8067d2c484abe75
elizafornia <elizavornia@gmail.com>
2026-09-15 12:37:50 +0700

    feat: update team page and add month picker

--- src/data.js
+++ src/data.js
@@ -58,6 +58,6 @@ export const TIM_ANGGOTA = [
   { id: "u1", nama: "Eliza", tim: "Data Management" },
   { id: "u2", nama: "Rani", tim: "Data Management" },
   { id: "u3", nama: "Dion", tim: "Data Management" },
-  { id: "u4", nama: "Faisal", tim: "Teknisi" },
-  { id: "u5", nama: "Arini", tim: "Teknisi" },
+  { id: "u4", nama: "Faisal", tim: "Data Management" },
+  { id: "u5", nama: "Arini", tim: "Data Management" },
 ];
